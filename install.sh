013ff8dfb620cd0e1510e3476d41c97b018ac683
David Bears <dbear4q@gmail.com>
2021-11-18 16:21:52 -0500

    make install script

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+echo "running some sanity checks..."
+for f in muteads.sh pauseafter.sh muteads.service.template
+  if ! [ -f $f ]; then
+    echo "$f does not exist. Make sure to run from the project directory."
+    exit 1
+  fi
+done
+echo "done running sanity chacks."
+
+
+echo "installing spotify-scripts..."
+
+echo "setting execute permission for scripts..."
+chmod a+x muteads.sh pauseafter.sh
+echo "done setting execute permission for scripts."
+
+
+echo "adding sym-links to user's bin..."
+if ! [ -d $HOME/bin ]; then
+  echo "user does not have a bin directory; creating one"
+  echo "shell must be restarted for custom commands to work"
+  mkdir $HOME/bin
+  needsrestart="yes"
+fi
+
+ln -s $HOME/bin/muteads `pwd`/muteads.sh
+ln -s $HOME/bin/pauseafter `pwd`/pauseafter.sh
+echo "done adding sym-links to user's bin."
+
+
+echo "creating systemd service file..."
+sed "s\\spotify-scripts\\`pwd`\\" muteads.service.template > muteads.service
+echo "done creating systemd service file."
+echo "starting systemd service..."
+systemctl --user --now enable `pwd`/muteads.service
+echo "done starting systemd service."
+
+echo "done installing spotify-scripts."
