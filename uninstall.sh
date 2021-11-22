49f0c852ee4c5aa84d821499a8c1193f875fb8ce
David Bears <dbear4q@gmail.com>
2021-11-22 12:07:07 -0500

    stop systemd service in uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -5,5 +5,5 @@ rm $HOME/bin/{muteads,pausenext}
 echo "done removing scripts from user's bin."
 
 echo "disabling systemd service..."
-systemctl --user disable muteads.service
+systemctl --user --now disable muteads.service
 echo "done disabling systemd service."
